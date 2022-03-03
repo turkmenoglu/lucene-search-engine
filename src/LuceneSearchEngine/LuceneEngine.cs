5f87d4d6b2933705131a52ce28f78b5b42f61eea
SANLAB\ismail.turkmenoglu <ismail.turkmenoglu@sanlab.net>
2022-03-03 14:12:42 +0300

    Lucene search engine usage in c#

---
+++ src/LuceneSearchEngine/LuceneEngine.cs
@@ -0,0 +1,74 @@
+﻿using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.QueryParsers;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+using LuceneSearchEngine.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LuceneSearchEngine
+{
+    public class LuceneEngine
+    {
+        private Analyzer _Analyzer;
+        private Directory _Directory;
+        private IndexWriter _IndexWriter;
+        private IndexSearcher _IndexSearcher;
+        private Document _Document;
+        private QueryParser _QueryParser;
+        private Query _Query;
+        private string _IndexPath = @"C:\LuceneIndex";
+
+        public LuceneEngine()
+        {
+            _Analyzer = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30);
+            _Directory = FSDirectory.Open(_IndexPath);
+        }
+
+        public void AddToIndex(IEnumerable<Product> values)
+        {
+            using (_IndexWriter = new IndexWriter(_Directory, _Analyzer, IndexWriter.MaxFieldLength.UNLIMITED))
+            {
+                foreach (var loopEntity in values)
+                {
+                    _Document = new Document();
+
+                    foreach (var loopProperty in loopEntity.GetType().GetProperties())
+                    {
+                        // Dilersek indexlenmesini istemediğimiz bir alan varsa, kontrol ederek Field.Index.NO vermemiz yeterli olacaktır.
+                        _Document.Add(new Field(loopProperty.Name, loopProperty.GetValue(loopEntity).ToString(), Field.Store.YES, Field.Index.ANALYZED));
+
+                        _IndexWriter.AddDocument(_Document);
+                        _IndexWriter.Optimize();
+                        _IndexWriter.Commit();
+                    }
+                }
+            }
+        }
+
+        public List<Product> Search(string field, string keyword)
+        {
+            // Üzerinde arama yapmak istediğimiz field için bir query oluşturuyoruz.
+            _QueryParser = new QueryParser(Lucene.Net.Util.Version.LUCENE_30, field, _Analyzer);
+            _Query = _QueryParser.Parse(keyword);
+
+            using (_IndexSearcher = new IndexSearcher(_Directory, true))
+            {
+                List<Product> products = new List<Product>();
+                var result = _IndexSearcher.Search(_Query, 10);
+
+                foreach (var loopDoc in result.ScoreDocs.OrderBy(s => s.Score))
+                {
+                    _Document = _IndexSearcher.Doc(loopDoc.Doc);
+
+                    products.Add(new Product() { Id = System.Convert.ToInt32(_Document.Get("Id")), Name = _Document.Get("Name") });
+                }
+
+                return products;
+            }
+        }
+    }
+}
---
+++ src/LuceneSearchEngine/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using LuceneSearchEngine.Entities;
+
+namespace LuceneSearchEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LuceneEngine engine = new LuceneEngine();
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product() { Id = 1, Name = "Apple Iphone 6" });
+            products.Add(new Product() { Id = 2, Name = "MacBook Air" });
+            products.Add(new Product() { Id = 3, Name = "Sony Xperia Z Ultra" });
+            products.Add(new Product() { Id = 4, Name = "Samsung Ultra HD Tv" });
+            products.Add(new Product() { Id = 5, Name = "Asus Zenphone 6" });
+            products.Add(new Product() { Id = 6, Name = "Sony Xperia Z 3" });
+            products.Add(new Product() { Id = 7, Name = "Sony Playstation 3" });
+
+            // Oluşturmuş olduğumuz test dokümanlarını ilgili indexleme metotumuza gönderelim ve indexlesin. Bu metotu sadece bir kere çalıştırmamız yeterlidir, daha sonrasında ise kapatabilirsiniz.
+            engine.AddToIndex(products);
+
+            // Oluşturmuş olduğumuz index üzerinden Name field'ına bakarak ve içerisinde "Sony" geçen en uygun kelimeleri scoring işlemi yaparak bize getirecektir.
+            var result = engine.Search("Name", "Sony");
+        }
+    }
+}
